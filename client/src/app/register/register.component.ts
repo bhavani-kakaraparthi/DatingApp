a7ebc846310e4338a2a259896a729f259a78c499
bhavani-kakaraparthi <bhavanikakaraparthi@gmail.com>
2021-03-16 11:45:26 +0530

    End of Section 6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -6,16 +8,22 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() usersFromHomeComponent: any; //for parent to child communication i.e., from home to register componenet
+  //@Input() usersFromHomeComponent: any; //for parent to child communication i.e., from home to register componenet
   @Output() cancelRegister = new EventEmitter(); // for child to parent communication i.e., register to home component
   model: any = {};
 
-  constructor() { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe(response => {
+      console.log(response);
+      this.cancel();
+    }, error => {
+      console.log(error);
+      this.toastr.error(error.error);
+    })
   }
   cancel() {
     this.cancelRegister.emit(false);
