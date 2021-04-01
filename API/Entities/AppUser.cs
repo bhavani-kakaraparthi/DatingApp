1b5b1e2ffc08678a841fc853908e60242345be31
bhavani-kakaraparthi <bhavanikakaraparthi@gmail.com>
2021-04-01 12:53:17 +0530

    End of Section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -12,8 +12,8 @@ namespace API.Entities
         public byte[] PasswordSalt { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime LastActive { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
@@ -21,10 +21,10 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
-        public int GetAge()
-        {
-            return DateOfBirth.CalculateAge();
-        }
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
         
     }
 }
